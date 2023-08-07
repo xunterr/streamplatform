0f1b1f777ffc41f48acc7d95bc09cc48d49acfc7
xunterr <artem.oxotnik@gmail.com>
2023-08-07 20:06:29 +0300

    Unauthorized JSON response

--- stream/src/main/java/com/xunterr/stream/security/SecurityConfiguration.java
+++ stream/src/main/java/com/xunterr/stream/security/SecurityConfiguration.java
@@ -1,9 +1,15 @@
 package com.xunterr.stream.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.wimdeblauwe.errorhandlingspringbootstarter.UnauthorizedEntryPoint;
+import io.github.wimdeblauwe.errorhandlingspringbootstarter.mapper.ErrorCodeMapper;
+import io.github.wimdeblauwe.errorhandlingspringbootstarter.mapper.ErrorMessageMapper;
+import io.github.wimdeblauwe.errorhandlingspringbootstarter.mapper.HttpStatusMapper;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -24,15 +30,29 @@ public class SecurityConfiguration {
 	private final JwtAuthConverter jwtAuthConverter;
 
 	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+	public UnauthorizedEntryPoint unauthorizedEntryPoint(HttpStatusMapper httpStatusMapper, ErrorCodeMapper errorCodeMapper,
+														 ErrorMessageMapper errorMessageMapper, ObjectMapper objectMapper) {
+
+		return new UnauthorizedEntryPoint(httpStatusMapper, errorCodeMapper, errorMessageMapper, objectMapper);
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http, UnauthorizedEntryPoint unauthorizedEntryPoint) throws Exception{
 		http
 				.csrf().disable()
 				.authorizeHttpRequests()
 				.requestMatchers("/api/v1/events/**").permitAll()
 				.anyRequest().authenticated();
+
+		http
+				.exceptionHandling()
+				.authenticationEntryPoint(unauthorizedEntryPoint);	//default auth entry point for Spring Security
+
 		http
 				.oauth2ResourceServer()
-				.jwt();
+				.jwt(Customizer.withDefaults())
+				.authenticationEntryPoint(unauthorizedEntryPoint); //for AuthenticationFailureHandler
+
 		http
 				.sessionManagement()
 				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
